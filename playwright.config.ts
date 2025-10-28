a2941d8fb785ee83436a4884ba609dd16d317e03
Alex Finnarn <alexander.finnarn@du.edu>
2025-10-28 09:58:03 -0400

    need to specify tests directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  testDir: './',
   testMatch: [
     '**/e2e/**/*.spec.ts',
     '**/web/modules/custom/**/tests/**/*.spec.ts'
