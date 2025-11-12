d32e0ab0a9413ba13017567c2f181140ed6088f0
Alex Finnarn <alexander.finnarn@du.edu>
2025-11-12 13:14:59 -0500

    try changing paths of cookies and data

--- tests/playwright/support/files.ts
+++ tests/playwright/support/files.ts
@@ -3,3 +3,7 @@ import path from 'path';
 export const getAssetPath = (relativePath: string) => {
   return path.join(__dirname, '..', 'assets', relativePath);
 };
+
+export const getDataPath = (relativePath: string) => {
+  return path.join(__dirname, '..', 'data', relativePath);
+};
