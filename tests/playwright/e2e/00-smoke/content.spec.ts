877c1b30f8a9b0e8aba1b6e6f4c8c8f9ed5d1e5d
Alex Finnarn <alexander.finnarn@du.edu>
2025-11-12 16:11:02 -0500

    test changes

--- tests/playwright/e2e/00-smoke/content.spec.ts
+++ tests/playwright/e2e/00-smoke/content.spec.ts
@@ -8,43 +8,46 @@ test.describe('@smoke - Basic Page Tests', () => {
   const site_admin = getRole('site_admin');
   const page_title = faker.lorem.words(3);
   const h1_text = faker.lorem.words(3);
+  const body_text = faker.lorem.paragraphs(1);
 
-
-  test('Test cookie login', async ({ page, context }) => {
-    await logIn(page, context, site_admin);
-    await page.goto('/node/add/page');
-
-    await page.getByLabel('Title', {exact: true}).fill(page_title);
-    await page.getByLabel('Alternative H1').fill(h1_text);
-  })
-
-  test.skip('Create basic page', async ({ page, context }) => {
+  test('Create basic page', async ({ page, context }) => {
     await logIn(page, context, site_admin);
     await page.goto('/node/add/page');
 
     await page.getByLabel('Title', {exact: true}).fill(page_title);
     await page.getByLabel('Alternative H1').fill(h1_text);
 
-    // Open hero image paragraph.
-    await page.getByRole('button', { name: 'Add Hero Media' }).click();
-    // Open the hero image file field.
-    await page.getByRole('button', { name: 'Hero Image' }).click();
-    // Open media library.
-    await page.locator('.field--name-field-hero-media-header')
-      .getByRole('button', { name: 'Select files' })
-      .click();
+    await page.getByRole('button', { name: 'Add Body Text', exact: true }).click();
+    // await fillParagraphField(page, 0, 'field-body-text', body_text);
 
-    const iframe = page.frameLocator('iframe[name="entity_browser_iframe_browse_files_modal"]');
-    // The button has onclick="event.preventDefault()" so just clicking should work.
-    await iframe.locator('a.button:has-text("Select file")').click();
+    // The contenteditable div that appears when CKEditor is active
+    const editor = page.locator('.ck-editor__editable[role="textbox"]').first();
+    await editor.fill(body_text);
 
-    //.setInputFiles('tests/e2e/fixtures/test.jpg');
-
+    // // Open hero image paragraph.
+    // await page.getByRole('button', { name: 'Add Hero Media' }).click();
+    // // Open the hero image file field.
+    // await page.getByRole('button', { name: 'Hero Image' }).click();
+    // // Open media library.
+    // await page.locator('.field--name-field-hero-media-header')
+    //   .getByRole('button', { name: 'Select files' })
+    //   .click();
+    // const iframe = page.frameLocator('iframe[name="entity_browser_iframe_browse_files_modal"]');
+    // // The button has onclick="event.preventDefault()" so just clicking should work.
+    // await iframe.locator('a.button:has-text("Select file")').click();
+    // //.setInputFiles('tests/e2e/fixtures/test.jpg');
 
     await page.getByRole('button', { name: 'Save' }).click();
     await page.waitForURL('/node/*');
-    // Expect title to be the h1 on the page.
-    await expect(page.getByRole('heading', { name: page_title })).toBeVisible();
-    await logOutViaUi(page);
+
+    // End up on admin/content where we can test the page title, which is different from the H1 text.
+    await page.getByRole('link', { name: page_title, exact: true }).click({ force: true });
+    await page.waitForURL('/node/*');
+
+    // Expect alternate H1 to be visible.
+    await expect(page.getByRole('heading', { name: h1_text })).toBeVisible();
+
+    // Expect body text to be what was entered in the field.
+    await expect(page.getByText(body_text)).toBeVisible();
   });
 });
