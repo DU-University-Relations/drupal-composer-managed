60291a5b04f9d0fef68e2cd3818b277a412d0df7
Alex Finnarn <alexander.finnarn@du.edu>
2025-11-19 10:51:28 -0500

    add PW test, fix profile PW auth issue

--- web/profiles/custom/ducore/modules/custom/du_logging/tests/playwright/e2e/du_logging.spec.ts
+++ web/profiles/custom/ducore/modules/custom/du_logging/tests/playwright/e2e/du_logging.spec.ts
@@ -1,17 +1,50 @@
 import { test, expect } from '@du_pw/test';
 import { faker } from '@faker-js/faker';
 
-import { getRole , logInViaForm, logOutViaUi} from "@du_pw/support/users";
+import { getRole , logIn, createAnonSession, logInViaForm} from "@du_pw/support/users";
+import {runDrushCommand} from "@du_pw/support/drush";
+import {run} from "node:test";
 
 test.describe('@du_logging - Log filtering tests', () => {
-  const site_admin = getRole('site_admin');
+  const administrator = getRole('administrator');
+  const dblog_page = '/admin/reports/dblog';
 
-  test.beforeEach(async ({ page }) => {
-
+  test.beforeAll(async () => {
+    runDrushCommand('en du_logging -y');
   });
 
   test('Filter logs, then disable filtering', async ({ page, context }) => {
-    await logInViaForm(page, context, site_admin);
+    await logIn(page, context, administrator);
+    const anonPage = await createAnonSession(context);
+    runDrushCommand('watchdog:delete all -y');
 
+    // Logging should be enabled, so go to two routes to test.
+    await anonPage.goto(dblog_page);
+    await anonPage.goto('/foo/bar/baz');
+
+    // Admin user should not see messages.
+    await page.goto(dblog_page);
+    await expect(page.getByText('No log messages available.')).toBeVisible();
+
+    // Disable filtering and see messages again.
+    runDrushCommand('cset du_logging.settings enabled 0 -y');
+    runDrushCommand('watchdog:delete all -y');
+
+    // Have anon user go to two routes to test.
+    await anonPage.goto(dblog_page);
+    await anonPage.goto('/foo/bar/baz');
+
+    // dmin user should see messages.
+    await page.goto(dblog_page);
+    await expect(
+      page.getByRole('table').getByRole('cell', { name: 'access denied' }).first()
+    ).toBeVisible();
+    await expect(
+      page.getByRole('table').getByRole('cell', { name: 'page not found' }).first()
+    ).toBeVisible();
   });
+
+  test.afterAll(async () => {
+    runDrushCommand('pmu du_logging -y');
+  })
 });
